0426e003735bf1ba3ba78d1305a45a5e9e79f45c
EMMANUEL <eodor02@gmail.com>
2024-04-29 10:03:41 +0100

    updated

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,7 +1,5 @@
 # The Selection Sort algorithm finds the lowest value in an array and moves it to the front of the array.
 #The algorithm looks through the array again and again, moving the next lowest values to the front, until the array is sorted.
-
-
 my_array2 = [64, 34, 25, 5, 22, 11, 90, 12]
 n = len(my_array2)
 for i in range(n-1):
@@ -13,3 +11,22 @@ for i in range(n-1):
     my_array2.insert(i, min_value2)
 
 print("Sorted2 array4:", my_array2)
+
+
+
+
+
+#Here is an implementation of the improved Selection Sort, using swapping:
+
+my_array = [64, 34, 25, 12, 22, 11, 90, 5]
+
+n = len(my_array)
+for i in range(n):
+    min_index = i
+    for j in range(i+1, n):
+        if my_array[j] < my_array[min_index]:
+            min_index = j
+        my_array[i], my_array[min_index] = my_array[min_index],my_array[i]
+
+
+print("Sorted4 array:", my_array)
